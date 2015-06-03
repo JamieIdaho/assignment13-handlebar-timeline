ab1e8fb3c467fce19fafa2304acea9cf34cbd76a
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-06-02 20:31:40 -0400

    fixed triangles in max css

--- js/templates.js
+++ js/templates.js
@@ -2,7 +2,7 @@ this["template"] = this["template"] || {};
 this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "  <div class=\"aboveContent\">\n\n  <div class=\"dateTime\">\n  <div class=\"date\">"
+  return "  <div class=\"box\">\n  <div class=\"aboveContent\">\n\n  <div class=\"dateTime\">\n  <div class=\"date\">"
     + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
     + "</div>\n  <div class=\"times\"> "
     + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
@@ -12,11 +12,11 @@ this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,p
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</div>\n  <div class=\"content\">"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</div>\n  </div>\n";
+    + "</div>\n  </div><!--end of titleContent div-->\n  </div><!--end of box div -->\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
   return "\n\n\n<div class=\"all\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.timelines : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "</div>\n\n\n";
+    + "</div><!--end of all div-->\n\n\n";
 },"useData":true});
